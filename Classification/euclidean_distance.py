d357d0c3ef86211d00ee142ff3ab9e540a15e83f
Nanofication <nansquared@gmail.com>
2017-06-09 11:17:47 -0400

    Made own version of K nearest

--- Classification/euclidean_distance.py
+++ Classification/euclidean_distance.py
@@ -1,8 +1,41 @@
+import numpy as np
 from math import sqrt
+import matplotlib.pyplot as plt
+import warnings
+from matplotlib import style
+from collections import Counter
 
-plot1 = [1,3]
-plot2 = [2,5]
+import pandas as pd
+import random
 
-euclidean_distance = sqrt((plot1[0] - plot2[0])**2 + (plot1[1] - plot2[1])**2)
+style.use('fivethirtyeight')
 
-print(euclidean_distance)
+dataset = {'k':[[1,2],[2,3],[3,1]], 'r': [[6,5],[7,7],[8,6]]}
+
+new_features = [5,7]
+
+for i in dataset:
+    for ii in dataset[i]:
+        plt.scatter(ii[0], ii[1], s = 100, color=i)
+
+# plt.show()
+
+def k_nearest_neighbors(data, predict, k=3):
+    if len(data) >= k:
+        warnings.warn('K is set to a value less than total voting groups!')
+    distances = []
+    for group in data:
+        for features in data[group]:
+            euclidean_distance = calculate_eculidean_distance(features, predict)
+            distances.append([euclidean_distance, group])
+
+    votes = [i[1] for i in sorted(distances)[:k]]
+    print(Counter(votes).most_common(1))
+    vote_result = Counter(votes).most_common(1)[0][0]
+
+    return vote_result
+
+def calculate_eculidean_distance(features, predict):
+    return np.linalg.norm(np.array(features) - np.array(predict))
+
+
